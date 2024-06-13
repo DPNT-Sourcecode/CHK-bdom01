5998e7c246670df99ce54146f8efd8f53349e0af
sbx_user1051 <sbx_user1051@169.254.83.165>
2024-06-13 15:43:35 +0000

    Thu Jun 13 15:43:35 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,5 +6,7 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
         
     def test_x_less_than_zero_raises_value_error(self):
-        assert sum_solution(-1, 3)
+        test_value = -1
+        assert sum_solution.compute(test_value, 3) == ValueError(f"x={test_value} is not of type int")
+
 
