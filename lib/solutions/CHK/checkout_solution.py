f1a45557dd616ae8f1eec80c771599ffeafff24e
sbx_user1051 <sbx_user1051@169.254.83.165>
2024-06-13 16:08:31 +0000

    Thu Jun 13 16:08:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,26 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # raise NotImplementedError()
+    count_items = Counter(skus)
+    total_price = 0
+    for item in ("A", "B", "C", "D"):
+        if item=="A":
+            offers_price = (count_items["A"] // 3) * 130
+            no_offers_price = (count_items["A"] % 3) * 50
+            total_price += offers_price
+            total_price += no_offers_price
+        elif item == "B":    
+            offers_price = (count_items["B"] // 2) * 45
+            no_offers_price = (count_items["B"] % 2) * 30
+            total_price += offers_price
+            total_price += no_offers_price
+        elif item == "C":
+            total_price += 20 * count_items["C"]
+            
+        elif item == "D":
+            total_price += 15 * count_items["D"]
+
+    return total_price
     
-    items = list(skus)
     
 
