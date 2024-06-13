205416999543c22bbb03e849154fa5b79eab275c
sbx_user1051 <sbx_user1051@169.254.83.165>
2024-06-13 15:19:11 +0000

    Thu Jun 13 15:19:11 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,11 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+    # raise NotImplementedError()
+    
+    if not isinstance(friend_name, str):
+        raise ValueError(f"{friend_name} is not of type string")
+    
+    return friend_name
+
