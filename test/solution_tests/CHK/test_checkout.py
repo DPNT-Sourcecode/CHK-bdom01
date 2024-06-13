088ca937b65bc23c202247afc3d3b0bb4943cab0
sbx_user1051 <sbx_user1051@169.254.83.165>
2024-06-13 16:23:31 +0000

    Thu Jun 13 16:23:31 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,8 +3,25 @@ import unittest
 
 
 class TestSum(unittest.TestCase):
-    def test_sum(self):
+    def test_basic(self):
         test_input = "ABCD"
         test_output = 115
         self.assertEqual(checkout_solution.checkout(test_input), test_output)
     
+    def test_valid_input_with_offers(self):
+        self.assertEqual(checkout_solution.checkout("AAABBBCCDD"), 130 + 45 + 30 + 40 + 30)
+    
+    def test_invalid_characters(self):
+        self.assertEqual(checkout_solution.checkout("AAABBBCCDDX"), -1)
+        
+    def test_empty_string(self):
+        self.assertEqual(checkout_solution.checkout(""), 0)
+        
+    def test_non_string_input(self):
+        self.assertEqual(checkout_solution.checkout(123), -1)
+        
+    def test_no_offers(self):
+        self.assertEqual(checkout_solution.checkout("ABCD"), 50 + 30 + 20 + 15)
+        
+    def test_only_offers(self):
+        self.assertEqual(checkout_solution.checkout("AAAABB"), 130 + 50 + 45)
