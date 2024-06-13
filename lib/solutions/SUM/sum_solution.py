234812026d70026ab1031968edfe4238c9160831
sbx_user1051 <sbx_user1051@169.254.83.165>
2024-06-13 15:08:59 +0000

    Thu Jun 13 15:08:59 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,10 +2,16 @@
 def compute(x: int, y: int) -> int:
     # raise NotImplementedError()
     
+    if not isinstance(x, int):
+        raise ValueError(f"x={x} is not of type int")
+    if not isinstance(y, int):
+        raise ValueError(f"y={x} is not of type int")
+    
     if x > 100 or x < 0:
-        raise ValueError(f"x={x} is not between 0 - 100")
-    elif y > 100:
-        raise ValueError(f"y={y} is not between 0 - 100")
+        raise ValueError(f"x={x} is not an integer between 0 - 100")
+    if y > 100 or y < 0:
+        raise ValueError(f"y={y} is not an integer between 0 - 100")
     
     return x + y
+
 
